9216d8386f19da16e10a73eb24ddab7063153afe
shaossword <shaos@gmx.fr>
2019-03-15 15:55:09 +0100

    ajout nav ,footer et home

---
+++ Site/structure/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+    
+        <?php
+        echo "j'aime le jambon et le saucisson - nicolas perez";
+        ?>
+        <p>j'aime les huitres</p>
+    </body>
+</html>
+
