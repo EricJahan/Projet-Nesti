87f52fda4707d829f012888ec5bee08cfa2110ad
shaossword <shaos@gmx.fr>
2019-03-19 11:25:10 +0100

    debut recette.php

--- Site/structure/vue/recette.php
+++ Site/structure/vue/recette.php
@@ -3,21 +3,27 @@
   <!----middle------>
   <div class="middle-recette">
     <ul class="recette-gauche">
-     <li>saison</li>
+      <div class="pre">saison
+      <ul class="saison">
+      <li><a href="#">printemp</a></li>
+     <li><a href="#">ete</a></li>
+     <li><a href="#">automne</a></li>
+     <li><a href="#">hiver</a></li>
+     </ul>
+     </div>
       <li>repas</li>
      <li>dessert</li>
     </ul>
     <div class="middle-home">
      <div class="b1"><a href=""> bob</a></div>
-     <div class="b2"><a href=""> joe</a></div>
+     <div class="b1"><a href=""> joe</a></div>
+     <div class="b1"><a href=""> bob</a></div>
+     <div class="b1"><a href=""> joe</a></div>
+     <div class="b1"><a href=""> bob</a></div>
+     <div class="b1"><a href=""> joe</a></div>
+     <div class="b1"><a href=""> bob</a></div>
+     <div class="b1"><a href=""> joe</a></div>
     </div>
-    <div class="middle-home">
-      <div class="b1"><a href=""> bob</a></div>
-      <div class="b2"><a href=""> joe</a></div>
-    </div>
-    <div class="middle-home">
-      <div class="b1"><a href=""> bob</a></div>
-      <div class="b2"><a href=""> joe</a></div>
-    </div>
+
   </div>
 </div>
