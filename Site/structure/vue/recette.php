497af049a731caa5c0dd16e8d9692417cc177607
shaossword <shaos@gmx.fr>
2019-03-19 13:29:43 +0100

    cscxsc

--- Site/structure/vue/recette.php
+++ Site/structure/vue/recette.php
@@ -3,16 +3,30 @@
   <!----middle------>
   <div class="middle-recette">
     <ul class="recette-gauche">
-      <div class="pre">saison
+      <div class="pre">Saison
       <ul class="saison">
-      <li><a href="#">printemp</a></li>
-     <li><a href="#">ete</a></li>
-     <li><a href="#">automne</a></li>
-     <li><a href="#">hiver</a></li>
+      <li><a href="#">Printemps</a></li>
+     <li><a href="#">Ete</a></li>
+     <li><a href="#">Automne</a></li>
+     <li><a href="#">Hiver</a></li>
      </ul>
      </div>
-      <li>repas</li>
-     <li>dessert</li>
+     <div class="pre">repas
+      <ul class="repas">
+      <li><a href="#">Prin</a></li>
+     <li><a href="#">Ee</a></li>
+     <li><a href="#">omne</a></li>
+     <li><a href="#">Hiv</a></li>
+     </ul>
+     </div>
+     <div class="pre">dessert
+      <ul class="dessert">
+      <li><a href="#">emps</a></li>
+     <li><a href="#">Et</a></li>
+     <li><a href="#">Auto</a></li>
+     <li><a href="#">Her</a></li>
+     </ul>
+     </div>
     </ul>
     <div class="middle-home">
      <div class="b1"><a href=""> bob</a></div>
