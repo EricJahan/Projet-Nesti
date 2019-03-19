6ecb1498e3fa12bf45c22a1c3ac989fffa1fbbd2
rubenavone <navone_ruben@msn.com>
2019-03-19 09:45:00 +0100

    changement mineur css

---
+++ Site/structure/vue/recette.php
@@ -0,0 +1,23 @@
+
+
+  <!----middle------>
+  <div class="middle-recette">
+    <ul class="recette-gauche">
+     <li>saison</li>
+      <li>repas</li>
+     <li>dessert</li>
+    </ul>
+    <div class="middle-home">
+     <div class="b1"><a href=""> bob</a></div>
+     <div class="b2"><a href=""> joe</a></div>
+    </div>
+    <div class="middle-home">
+      <div class="b1"><a href=""> bob</a></div>
+      <div class="b2"><a href=""> joe</a></div>
+    </div>
+    <div class="middle-home">
+      <div class="b1"><a href=""> bob</a></div>
+      <div class="b2"><a href=""> joe</a></div>
+    </div>
+  </div>
+</div>
