a384f9e0d89a3baf7846b04ba2b90a1b22b34c0b
Eric Jahan <ThunderMephisto@MBPdeericjahan.localdomain>
2019-03-26 16:08:12 +0100

    bug

--- Site/structure/vue/uneRecette.php
+++ Site/structure/vue/uneRecette.php
@@ -1,28 +1,29 @@
 <div class="uneRecette">
-<div class="middle-uneRecette">
-    <div class="ingrediant">
-    <ul>
-        <li>ss</li>
-        <li>ss</li>
-        <li>ss</li>
-    </ul>
-    </div>
-    <div class="recette">
-    <img src="img/pain.jpg" alt="">
-    </div>
-    <div class="middle-uneRectteDeux">
-    <div>
-    
-        <div class="b1">
-            <h1> <?php print $recipe->getNom(); ?> </h1>
-            <br />
-            <p> Nombre de personne <?php print $recipe->getQuantite(); ?></p>
-            <br />
-            <p> Temps requis <?php print $recipe->getTemp(); ?> </p>
-            <br />
-            <p> Descriptions<?php print $recipe->getDescription(); ?> </p>
+    <div class="middle-uneRecette">
+        <div class="ingrediant">
+            <ul>
+                <li>ss</li>
+                <li>ss</li>
+                <li>ss</li>
+            </ul>
+        </div>
+        <div class="recette">
+            <img src="img/pain.jpg" alt="">
+        </div>
+        <div class="middle-uneRectteDeux">
+            <div>
+
+                <div class="b1">
+                    <h1> <?php print $recipe->getNom(); ?> </h1>
+                    <br />
+                    <p> Nombre de personne <?php print $recipe->getQuantite(); ?></p>
+                    <br />
+                    <p> Temps requis <?php print $recipe->getTemp(); ?> </p>
+                    <br />
+                    <p> Descriptions<?php print $recipe->getDescription(); ?> </p>
+                </div>
+                
+            </div>
         </div>
     </div>
-    
-</div>
 </div>
