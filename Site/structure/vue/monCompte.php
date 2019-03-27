462ec85c6dfacf947d0f0bd476640668ad30d5c1
shaossword <shaos@gmx.fr>
2019-03-27 16:48:46 +0100

    push pour faire plaisir sexe

---
+++ Site/structure/vue/monCompte.php
@@ -0,0 +1,57 @@
+<section class="mon-compte global-height">
+    <form class="form-compte" action="form.php" method="post">
+        <h3 class="mon-compte-titre">Coordonnées</h3>
+        <br>
+        <label for=""> Nom:<input type="submit" value="Modifier">
+            <input type="text" name="nom">
+        </label>
+
+        <label for="">Prénom:<input type="submit" value="Modifier">
+            <input type="text" name="prenom">
+        </label>
+        <br>
+        <br>
+        <h3>Adresse</h3>
+        <br>
+        <label for="">N°:
+            <input type="text" name="mail">
+        </label>
+
+        <label for="">Rue:
+            <input type="text" name="mail">
+        </label>
+        <label for="">CP:
+            <input type="text" name="mail">
+        </label>
+
+        <label for="">Ville:
+            <input type="text" name="mail">
+        </label>
+        <label for="">Pays:
+            <input type="text" name="mail">
+        </label>
+
+        <input type="submit" value="Modifier">
+
+    </form>
+    <form class="form-compte" action="form.php" method="post">
+        <h3 class="mon-compte-titre">Compte</h3>
+        <br>
+        <label for=""> Adresse Mail:
+            <input type="text" name="nom">
+        </label>
+
+        <label for="">Ancien mot de passe:
+            <input type="text" name="prenom">
+        </label>
+
+        <label for="">Nouveau mot de passe:
+            <input type="text" name="mail">
+        </label>
+
+        <label for="">Verification mot de passe:
+            <input type="text" name="mail">
+        </label>
+        <input type="submit" value="Modifier mot de passe">
+    </form>
+</section> 
